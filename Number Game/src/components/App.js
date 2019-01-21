7706e56db28a0f2d29d290c5af32e9dc1f50fc05
nimitp <npatel1@ufl.edu>
2019-01-20 19:55:58 -0500

    game reset added

--- Number Game/src/components/App.js
+++ Number Game/src/components/App.js
@@ -2,9 +2,25 @@ import React from 'react';
 import Game from './Game';
 
 class App extends React.Component{
+
+    state = {
+        gameId: 1,
+    };
+
+    reset = () =>{
+        this.setState((prevState) =>{
+            return {gameId : prevState.gameId + 1};
+        });
+    };
+
     render(){
         return(
-            <Game randomNumberCount = {6}/>
+            <Game 
+                key = {this.state.gameId} 
+                randomNumberCount = {6} 
+                initialSeconds = {10}
+                onPlayAgain = {this.reset}
+            />
         );
     }
 }
